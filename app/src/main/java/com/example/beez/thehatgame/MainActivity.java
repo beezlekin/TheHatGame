85eda0bebaccfcb1daea23ee9813964650a38437
Mitchel Kuzniar <beezlekin@gmail.com>
2018-02-06 18:15:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/beez/thehatgame/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.beez.thehatgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button play, rules;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        play = findViewById(R.id.button);
+        play.setOnClickListener(this);
+        rules = findViewById(R.id.button2);
+        rules.setOnClickListener(this);
+
+    }
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.button:
+                Intent intent_play = new Intent(MainActivity.this,PlayActivity.class);
+                startActivity(intent_play);
+                break;
+            case R.id.button2:
+                Intent intent_rules = new Intent(MainActivity.this,RulesActivity.class);
+                startActivity(intent_rules);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/beez/thehatgame/PlayActivity.java
@@ -0,0 +1,30 @@
+package com.example.beez.thehatgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button
+
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
+    Button single, multi
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        single = findViewById(R.id.button4);
+        single.setOnClickListener(this);
+        multi = findViewById(R.id.button3);
+        multi.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button3:
+
+                break;
+            case R.id.button4:
+
+                break;
+        }
+    }
+}
